c0a86d7c932efbba8021237da49211555fbf8f26
Nicholas Tierney <nicholas.tierney@gmail.com>
2018-11-30 13:06:35 +1100

    update anna data

--- data-raw/create-oz-anna.R
+++ data-raw/create-oz-anna.R
@@ -3,8 +3,9 @@ library(ozbabynames)
 oz_anna <- 
   ozbabynames %>%
   filter(name == "Anna") %>%
-  group_by(sex, name) %>%
+  group_by(name, year) %>%
   summarise(count = sum(count)) %>%
   arrange(-count)
+
 
 readr::write_csv(oz_anna, here::here("data/oz_anna.csv"))
